ad66aa3141ad0a043d2938cf365decd0347a1f4c
Innocent <ndayikengurukiyeinnocent95@gmail.com>
2022-02-02 11:37:33 +0200

    Creation de la fonction Delete dans le controller TypeConsommation

--- GestionConsommationVehiculeMvc/Controllers/TypeConsommationController.cs
+++ GestionConsommationVehiculeMvc/Controllers/TypeConsommationController.cs
@@ -125,6 +125,7 @@ namespace GestionConsommationVehiculeMvc.Controllers
 
         protected override void Dispose(bool disposing)
         {
+
             db.Dispose();
             base.Dispose(disposing);
         }
